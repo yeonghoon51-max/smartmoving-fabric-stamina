4aeeb918e225b53624dd097ca4be7ca1397ef205
yeonghoon51-max <yeonghoon51@gmail.com>
2025-09-30 22:26:54 +0900

    Update ServerPlayerEntityAccessor.java

--- SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/ServerPlayerEntityAccessor.java
+++ SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/ServerPlayerEntityAccessor.java
@@ -1,1 +1,5 @@
-package com.yourpkg.smartmoving.mixin; public interface ServerPlayerEntityAccessor {}
+package com.yourpkg.smartmoving.mixin;
+
+public interface ServerPlayerEntityAccessor {
+    // 나중에 @Accessor/@Invoker가 필요하면 여기에 추가
+}

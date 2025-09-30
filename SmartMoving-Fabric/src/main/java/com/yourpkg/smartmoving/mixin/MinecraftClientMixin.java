d0d507ed9d486b098aa781880f901fc00215e3a9
yeonghoon51-max <yeonghoon51@gmail.com>
2025-09-30 22:26:06 +0900

    Update MinecraftClientMixin.java

--- SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/MinecraftClientMixin.java
+++ SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/MinecraftClientMixin.java
@@ -1,1 +1,16 @@
-package com.yourpkg.smartmoving.mixin; public class MinecraftClientMixin {}
+package com.yourpkg.smartmoving.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void smartmoving$tick(CallbackInfo ci) {
+        // 클라이언트 틱 로직이 필요하면 여기에 작성
+    }
+}

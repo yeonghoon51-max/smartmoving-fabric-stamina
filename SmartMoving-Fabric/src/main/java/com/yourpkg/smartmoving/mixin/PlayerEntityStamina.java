7009c7fb739049d384a3b300ac0f17c3e587b610
yeonghoon51-max <yeonghoon51@gmail.com>
2025-09-30 22:17:36 +0900

    Update PlayerEntityStamina.java

--- SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/PlayerEntityStamina.java
+++ SmartMoving-Fabric/src/main/java/com/yourpkg/smartmoving/mixin/PlayerEntityStamina.java
@@ -1,1 +1,9 @@
-package com.yourpkg.smartmoving.mixin; public class PlayerEntityStamina {}
+package com.yourpkg.smartmoving.mixin;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerEntityStamina {
+    // 여기에 나중에 스태미너 관련 필드/메소드, @Inject 같은 Mixin 코드가 들어갑니다.
+}

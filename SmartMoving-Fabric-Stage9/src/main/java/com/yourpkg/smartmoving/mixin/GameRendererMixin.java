80c5c722505daff94c0178de276abe372fe0b85c
yeonghoon51-max <yeonghoon51@gmail.com>
2025-10-04 11:44:01 +0900

    Delete SmartMoving-Fabric-Stage9 directory

--- SmartMoving-Fabric-Stage9/src/main/java/com/yourpkg/smartmoving/mixin/GameRendererMixin.java
+++
@@ -1,7 +0,0 @@
-package com.yourpkg.smartmoving.mixin;
-
-import net.minecraft.client.render.GameRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GameRenderer.class)
-public abstract class GameRendererMixin { }
--- SmartMoving-Fabric-Stage9/src/main/java/com/yourpkg/smartmoving/mixin/PlayerEntityModelMixin.java
+++
@@ -1,8 +0,0 @@
-package com.yourpkg.smartmoving.mixin;
-
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import net.minecraft.entity.LivingEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerEntityModel.class)
-public abstract class PlayerEntityModelMixin<T extends LivingEntity> { }

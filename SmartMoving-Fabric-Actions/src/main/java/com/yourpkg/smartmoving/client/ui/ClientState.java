0db93690dbd9f4c01c3120fd28c9da7de1f0cfb6
yeonghoon51-max <yeonghoon51@gmail.com>
2025-10-29 13:54:44 +0900

    Delete SmartMoving-Fabric-Actions directory

--- SmartMoving-Fabric-Actions/src/main/java/com/yourpkg/smartmoving/client/ui/ClientState.java
+++
@@ -1,9 +0,0 @@
-package com.yourpkg.smartmoving.client.ui;
-
-public class ClientState {
-    public static float  jumpCharge = 0f;
-    public static float  grabEnergy = 100f;
-    public static boolean grabbing  = false;
-    public static boolean crawling  = false;
-    public static boolean grabKeyDown = false;
-}

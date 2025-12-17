f9fed3d2574b197bcbc89067112dcd54b648aed7
yeonghoon51-max <yeonghoon51@gmail.com>
2025-12-17 11:11:28 +0900

    Make Mixin stub repeatable

--- SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
+++ SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
@@ -1,9 +1,14 @@
 package org.spongepowered.asm.mixin;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Repeatable(Mixins.class)
 public @interface Mixin {
     Class<?>[] value() default {};
     String[] targets() default {};

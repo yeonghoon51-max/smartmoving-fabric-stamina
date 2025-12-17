9abc4274a526afb82befcce836d3df04866f66c1
yeonghoon51-max <yeonghoon51@gmail.com>
2025-12-17 11:50:47 +0900

    Align stub mixin annotations with runtime behavior

--- SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
+++ SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
@@ -1,13 +1,17 @@
 package org.spongepowered.asm.mixin;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Repeatable(Mixins.class)
 public @interface Mixin {
     Class<?>[] value() default {};

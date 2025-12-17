a5ea581a1e909b6b7dc358de70b4db53a2f8eba5
yeonghoon51-max <yeonghoon51@gmail.com>
2025-12-17 10:49:41 +0900

    Target Minecraft 1.21.8 and fix mixin annotation stub

--- SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
+++ SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
@@ -1,6 +1,13 @@
 package org.spongepowered.asm.mixin;
 
 import java.lang.annotation.*;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Mixin { Class<?>[] value(); }
+public @interface Mixin {
+    Class<?>[] value() default {};
+    String[] targets() default {};
+    int priority() default 1000;
+    boolean remap() default true;
+    String[] constraints() default {};
+}

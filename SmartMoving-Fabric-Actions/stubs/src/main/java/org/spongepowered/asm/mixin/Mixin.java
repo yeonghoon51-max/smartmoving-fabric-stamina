93768a8ca8108e606df6e6cd55ad7fb7fb285a4d
yeonghoon51-max <yeonghoon51@gmail.com>
2026-03-06 18:44:27 +0900

    Match stub mixin retention to real Sponge Mixin

--- SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
+++ SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixin.java
@@ -1,18 +1,12 @@
 package org.spongepowered.asm.mixin;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
-@Inherited
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
-@Repeatable(Mixins.class)
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
 public @interface Mixin {
     Class<?>[] value() default {};
     String[] targets() default {};
--- SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixins.java
+++ SmartMoving-Fabric-Actions/stubs/src/main/java/org/spongepowered/asm/mixin/Mixins.java
@@ -1,15 +1,11 @@
 package org.spongepowered.asm.mixin;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
-@Inherited
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 public @interface Mixins {
     Mixin[] value();
